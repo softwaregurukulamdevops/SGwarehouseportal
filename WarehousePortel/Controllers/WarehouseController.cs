032c6c3233f6c694174835f91925cff2d3315caa
Kotagiri lokesh <ktss.kotagiri@gmail.com>
2024-08-19 00:19:33 +0530

    updated new files

---
+++ WarehousePortel/Controllers/WarehouseController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WarehousePortel.Models;
+
+namespace WarehousePortel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehouseController : Controller
+    {
+        public readonly TrainingDBContext trainingDBContext;
+        public WarehouseController(TrainingDBContext _trainingDBContext)
+        {
+            trainingDBContext = _trainingDBContext;
+        }
+        [HttpGet("GetWarehouseDetails")]
+        public List<Warehouse> GetWarehouseDetails()
+        {
+            List<Warehouse> lstWarehouse = new List<Warehouse>();
+            try
+            {
+                lstWarehouse = trainingDBContext.Warehouse.ToList();
+                return lstWarehouse;
+            }
+            catch (Exception ex)
+            {
+                lstWarehouse = new List<Warehouse>();
+                return lstWarehouse;
+            }
+        }
+        [HttpPost("AddWarehouse")]
+        public string AddWarehouse(Warehouse warehouse)
+        {
+            string message = string.Empty;
+            try
+            {
+                if (!string.IsNullOrEmpty(warehouse.WarehouseName))
+                {
+                    trainingDBContext.Add(warehouse);
+                    trainingDBContext.SaveChanges();
+                    message = "Warehouse added successfully";
+                }
+                else
+                    message = "Warehouse Name required.";
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            return message;
+        }
+    }
+}
---
+++ WarehousePortel/Models/TrainingDBContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WarehousePortel.Models
+{
+    public partial class TrainingDBContext : DbContext
+    {
+        public TrainingDBContext()
+        {
+        }
+
+        public TrainingDBContext(DbContextOptions<TrainingDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Department { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<Item> Item { get; set; }
+        public virtual DbSet<Warehouse> Warehouse { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=softwaregurukulam.database.windows.net;User ID=admin_sg;Password=SG@1234$;Database=training;Trusted_Connection=True;Integrated Security=false");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.DepartmentId).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.EmployeeId).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
